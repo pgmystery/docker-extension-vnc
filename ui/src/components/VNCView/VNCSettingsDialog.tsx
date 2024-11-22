f9559693a84ea78371d0f956de130020b96818c2
pgmystery <philipp.glaw@gmx.de>
2024-11-23 00:58:33 +0100

    Fix bug in vnc settings

--- ui/src/components/VNCView/VNCSettingsDialog.tsx
+++ ui/src/components/VNCView/VNCSettingsDialog.tsx
@@ -29,8 +29,6 @@ export default function VNCSettingsDialog({ open, close, settingsData, onSetting
   }, [reset])
 
   function save(data: VNCSettingsSaveData) {
-    console.log(data)
-
     onSettingChange({
       qualityLevel: Number(data.qualityLevel),
       compressionLevel: Number(data.compressionLevel),
