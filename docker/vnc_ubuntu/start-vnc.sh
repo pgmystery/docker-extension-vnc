a54588f848690cc2c36b67a8cbe492a769ae7463
pgmystery <philipp.glaw@gmx.de>
2024-11-27 17:24:24 +0100

    Fix bug in the example ubuntu_vnc docker image

--- docker/vnc_ubuntu/start-vnc.sh
+++ docker/vnc_ubuntu/start-vnc.sh
@@ -5,8 +5,13 @@ HOSTNAME=$(hostname)
 echo "127.0.1.1\t$HOSTNAME" >> /etc/hosts
 
 echo "Starting VNC server at $RESOLUTION..."
+vncserver -kill :0 || true
 vncserver -kill :1 || true
-vncserver -geometry $RESOLUTION &
+rm -f /tmp/.X0-lock
+rm -f /tmp/.X11-unix/X0
+rm -f /tmp/.X1-lock
+rm -f /tmp/.X11-unix/X1
+vncserver :1 -geometry $RESOLUTION -rfbport 5901 &
 
 echo "VNC server started at $RESOLUTION! ^-^"
 
