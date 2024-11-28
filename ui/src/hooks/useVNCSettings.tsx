1582037e7569bd460973edabd5cf8885d7e8fb94
pgmystery <philipp.glaw@gmx.de>
2024-11-28 23:10:29 +0100

    Add new vnc setting "view only"

--- ui/src/hooks/useVNCSettings.tsx
+++ ui/src/hooks/useVNCSettings.tsx
@@ -3,12 +3,14 @@ import { VNCSettingsData } from '../components/VNCView/VNCView'
 import { CompressionLevelRange } from '../components/VNCView/VNCSettingForms/CompressionLevel'
 import { QualityLevelRange } from '../components/VNCView/VNCSettingForms/QualityLevel'
 import { ShowDotCursorDefault } from '../components/VNCView/VNCSettingForms/ShowDotCursor'
+import { ViewOnlyDefault } from '../components/VNCView/VNCSettingForms/ViewOnly'
 
 
 const defaultSettings: VNCSettingsData = {
   qualityLevel: QualityLevelRange.default,
   compressionLevel: CompressionLevelRange.default,
   showDotCursor: ShowDotCursorDefault,
+  viewOnly: ViewOnlyDefault,
 }
 
 
