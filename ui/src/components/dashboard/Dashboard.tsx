bd4572cf42f88ea2d772c1e79db7c3a23f6c560a
pgmystery <philipp.glaw@gmx.de>
2024-12-12 15:44:16 +0100

    Fix bug

--- ui/src/components/dashboard/Dashboard.tsx
+++ ui/src/components/dashboard/Dashboard.tsx
@@ -140,7 +140,7 @@ export default function Dashboard({ ddUIToast, connect, sessionStore }: Dashboar
         connection: {
           type: 'container',
           data: {
-            container: exampleContainer.Id,
+            container: exampleContainer.Name,
             port: UbuntuVNCDockerImagePort,
           },
         },
