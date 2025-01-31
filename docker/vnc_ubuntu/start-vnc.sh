bb209e8144d94edfb45179bb5670fbc937d6d031
Philipp G. <philipp.glaw@gmx.de>
2025-01-31 05:55:59 +0100

    Update start-vnc.sh

--- docker/vnc_ubuntu/start-vnc.sh
+++ docker/vnc_ubuntu/start-vnc.sh
@@ -9,10 +9,7 @@ chmod 600 /root/.vnc/passwd
 
 echo "Starting tiger VNC server at $RESOLUTION..."
 tigervncserver -kill :0 || true
-tigervncserver -kill :1 || true
 rm -f /tmp/.X0-lock
 rm -f /tmp/.X11-unix/X0
-rm -f /tmp/.X1-lock
-rm -f /tmp/.X11-unix/X1
 
-tigervncserver :1 -geometry "$RESOLUTION" -rfbport "$PORT" -localhost no -fg $*
+tigervncserver :0 -geometry "$RESOLUTION" -rfbport "$PORT" -localhost no -fg $*
