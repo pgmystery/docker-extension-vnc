33083c6fc6011b554a3fe53ef602d4eec4e572f7
pgmystery <philipp.glaw@gmx.de>
2025-04-20 23:43:24 +0200

    Update the clipboard menu to handle a large amount of text better. Make version 1.3.1

--- ui/src/components/inputs/ClipboardField.tsx
+++ ui/src/components/inputs/ClipboardField.tsx
@@ -1,4 +1,5 @@
 import { styled, TextareaAutosize, TextField } from '@mui/material'
+import { useEffect, useRef } from 'react'
 
 interface ClipboardFieldProps {
   value: string
@@ -8,6 +9,35 @@ interface ClipboardFieldProps {
 }
 
 export default function ClipboardField({ isPassword, autoFocus, value, setValue }: ClipboardFieldProps) {
+  const textAreaRef = useRef<HTMLTextAreaElement>(null)
+  const resizeObserverRef = useRef<ResizeObserver | null>(null)
+
+  useEffect(() => {
+    const textAreaElement = textAreaRef.current
+    if (!textAreaElement) return
+
+    const updateTopPosition = () => {
+      const top = textAreaElement.getBoundingClientRect().top
+
+      document.documentElement.style.setProperty('--textarea-top', `${top}px`)
+    }
+
+    updateTopPosition()
+
+    if (!resizeObserverRef.current) {
+      resizeObserverRef.current = new ResizeObserver(() => {
+        updateTopPosition()
+      })
+    }
+
+    resizeObserverRef.current.observe(textAreaElement)
+
+    return () => {
+      if (resizeObserverRef.current && textAreaElement) {
+        resizeObserverRef.current.unobserve(textAreaElement)
+      }
+    }
+  }, [])
 
   if (isPassword) {
     return (
@@ -16,15 +46,20 @@ export default function ClipboardField({ isPassword, autoFocus, value, setValue 
         type="password"
         value={value}
         onChange={e => setValue(e.currentTarget.value)}
+        sx={{
+          width: '300px',
+        }}
       />
     )
   }
   else {
     return (
       <Textarea
+        ref={textAreaRef}
         autoFocus={autoFocus}
         value={value}
         onChange={e => setValue(e.currentTarget.value)}
+        placeholder="Edit clipboard content in here..."
       />
     )
   }
@@ -32,4 +67,7 @@ export default function ClipboardField({ isPassword, autoFocus, value, setValue 
 
 const Textarea = styled(TextareaAutosize)(() => `
   min-height: 50px;
+  min-width: 300px;
+  overflow: auto !important;
+  max-height: calc(100vh - var(--textarea-top, 0));
 `)
