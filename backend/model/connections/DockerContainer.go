3b4f28a48472ae64793aa92432567c1682301a48
pgmystery <philipp.glaw@gmx.de>
2025-06-10 08:55:46 +0200

    With Docker Desktop version >=4.42.0, the response data of the request to the backend changed

--- backend/model/connections/DockerContainer.go
+++ backend/model/connections/DockerContainer.go
@@ -7,9 +7,10 @@ import (
 
 type DockerContainer struct {
 	gorm.Model
-	ID        uuid.UUID `gorm:"type:uuid;primary_key;" json:"id"`
-	Container string    `gorm:"type:varchar(255);not null;" json:"container"`
-	Port      uint      `gorm:"not null;" json:"port"`
+	ID                  uuid.UUID `gorm:"type:uuid;primary_key;" json:"id"`
+	Container           string    `gorm:"type:varchar(255);not null;" json:"container"`
+	Port                uint      `gorm:"not null;" json:"port"`
+	StopAfterDisconnect bool      `gorm:"default:false;" json:"stopAfterDisconnect"`
 }
 
 func (dockerContainer *DockerContainer) BeforeCreate(_ *gorm.DB) (err error) {
